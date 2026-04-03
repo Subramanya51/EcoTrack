6814127e1ddfc229b7a5fa1c94e4883bdca86bb8
Subramanya51 <subramanyakamble86@gmail.com>
2026-04-03 13:30:43 +0530

    fix /public path

--- src/main/java/com/SmartGarbageCollection/GarbageCollection/Controller/PublicController.java
+++ src/main/java/com/SmartGarbageCollection/GarbageCollection/Controller/PublicController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/public")
+@RequestMapping("/public")
 @RequiredArgsConstructor
 public class PublicController {
 
